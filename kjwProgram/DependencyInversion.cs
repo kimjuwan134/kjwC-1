7083cf8c6b5eccad5b581a4c54ca2607b6798a2e
kimjuwan134 <139342292+kimjuwan134@users.noreply.github.com>
2023-11-17 17:31:12 +0900

    1

--- kjwProgram/DependencyInversion.cs
+++ kjwProgram/DependencyInversion.cs
@@ -11,6 +11,63 @@ namespace kjwProgram
     // 상위 계층이 하위 계층에 의존하는 전통적인 관계를 반전시킴으로써
     // 상위 계층이 하위 계층의 구현으로부터 독립될 수 있도록 설계하는 원칙.
 
+    public abstract class Weapon
+    {
+        protected int attack;
+        protected string name;
+
+        public Weapon(int attack, string name)
+        {
+            this.attack = attack;
+            this.name = name;
+        }
+
+        public int Attack
+        {
+            get { return attack; } 
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public abstract void Action();
+    }
+
+    public class Axe : Weapon
+    {
+        private readonly int attack = 50;
+        protected string name;
+
+        public Axe() : base(10, "Axe")
+        {
+
+        }
+
+        public override void Action()
+        {
+            Console.WriteLine("Axe Action");
+        }
+    }
+
+    public class Knife : Weapon
+    {
+        public override void Action()
+        {
+            Console.WriteLine("Knife Action");
+        }
+
+    }
+
+    public class Rifle : Weapon
+    {
+        public override void Action()
+        {
+            Console.WriteLine("Rifle Action");
+        }
+    }
+
 
     #endregion
 
