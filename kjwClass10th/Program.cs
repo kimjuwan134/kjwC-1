e464c31dfbbf7aa661711b4111d8f8126a5a3994
hlf1 <139342292+kimjuwan134@users.noreply.github.com>
2023-11-22 19:47:47 +0900

    (2023.11.22) 무명 형식

---
+++ kjwClass10th/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace kjwClass10th
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 등차 수열
+
+            // 숙제 등차수열
+            /*
+            int N = 13;
+            int A = 5;
+            int B = 10;
+
+            for(int i = 0; i < B; i++)
+            {
+                A += N;
+                Console.WriteLine(A);
+            }
+            */
+
+            // 숙제 등차수열 풀이
+
+            //int firstTerm = 0; // 첫 항
+            //int commonDifferent = 0; // 공차
+            //int size = 0;
+
+            // Console.ReadLine() : string 타입으로만 값을 반환.
+            //string name = Console.ReadLine();
+
+            // Int32.Parse : int 타입으로 값을 반환.
+            //Console.Write("첫 항의 값 입력 : ");
+            //firstTerm = Int32.Parse(Console.ReadLine());
+
+            //Console.WriteLine("");
+
+            //Console.Write("공차의 값 입력 : ");
+            //commonDifferent = Int32.Parse(Console.ReadLine());
+
+            //Console.WriteLine("");
+
+            //Console.Write("n의 값 입력 : ");
+            //size = Int32.Parse(Console.ReadLine());
+
+
+            //Console.WriteLine("firstTerm 변수의 값 : " + firstTerm);
+            //Console.WriteLine("commonDifferent 변수의 값 : " + commonDifferent);
+            //Console.WriteLine("size 변수의 값 : " + size);
+
+            ////Console.WriteLine("name의 변수의 값 : " + name);
+
+            //for (int i = 0; i < size; i++)
+            //{
+            //    Console.WriteLine(firstTerm + commonDifferent * i);
+            //}
+
+            #endregion
+
+            #region 등비 수열
+
+            //int first;
+            //int different;
+            //int size;
+
+            //Console.Write("첫 항의 값 : ");
+            //first = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("");
+            //Console.Write("공비의 값 : ");
+            //different = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("");
+            //Console.Write("n의 값 : ");
+            //size = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("");
+
+            //for(int i = 1; i <= size; i++)
+            //{
+            //    Console.WriteLine(first);
+            //    first *= different;
+            //}
+
+            #endregion
+
+            #region 무명 형식
+
+            // 이름이 존재하지 않는 객체 또는 변수를 의미.
+
+            // 무명 형식의 경우 값을 읽기만 할 수 있음.
+            //var data = new { Name = "Data", Value = 10 };
+
+            //Console.WriteLine("data.Name : " + data.Name + " data.Value : " + data.Value);
+
+            #endregion
+        }
+    }
+}
--- kjwProgram/Program.cs
+++ kjwProgram/Program.cs
@@ -1,20 +1,63 @@
-﻿namespace kjwProgram
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace kjwProgram
 {
     internal class Program
     {
+        public void BinarySearch(int[] a, int b)
+        {
+            int pivot = a.Length / 2;
+            int left = 0;
+            int right = a.Length - 1;
+            
+            while(true)
+            {
+                if(a[pivot] == b)
+                {
+                    Console.WriteLine("자료 발견 : " + b);
+                }
+                else if (b < a[pivot])
+                {
+                    right = pivot;
+                    pivot = (left + right) / 2;
+                }
+                else if (b > a[pivot])
+                {
+                    left = pivot;
+                    pivot = (left + right) / 2;
+                }
+                else if()
+            }
+        }
+
         static void Main(string[] args)
         {
-            // 숙제 등차수열
+            #region 이진 탐색
 
-            int N = 6;
-            int A = 1;
-            int B = 10;
+            // 오름차순으로 정렬된 리스트에 특정한 값의 위치를 찾는 알고리즘.
 
-            for(int i = 0; i < B; i++)
-            {
-                A += N;
-                Console.WriteLine(A);
-            }
+            // 1. 배열의 가운데 요소의 인덱스를 pivot으로 설정.
+
+            // 2. [pivot]의 값이 찾고자 하는 요소와 같다면 검색 완료.
+
+            // 3. [pivot]의 값이 찾는 값보다 크다면 left ~ pivot 사이를 검색.
+
+            // 4. [pivot]의 값이 찾는 값보다 작다면 pivot ~ right 사이를 검색.
+
+            int[] array = new int[6] { 1, 6, 8, 13, 15, 20 };
+
+            
+
+            
+            
+
+
+            
+
+
+            #endregion
+
 
         }
     }
