3d01795ae960d43b4a386d9a48aaabe6503d4506
hlf1 <139342292+kimjuwan134@users.noreply.github.com>
2023-11-24 19:47:25 +0900

    (2023.11.24) 병합 정렬

---
+++ kjwClass12th/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace kjwClass12th
+{
+    internal class Program
+    {
+        static void Swap(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        static void QuickSort(int[] array, int start, int end)
+        {
+            // 1. left가 right 보다 크거나 같아졌을 때
+            if (start >= end) return;
+
+            // 2. pivot 변수값 설정.
+            int pivot = start;
+
+            // 3. left 변수값 설정.
+            int left = start + 1;
+
+            // 4. right 변수값 설정.
+            int right = end;
+
+            // 5. left가 right보다 크거나 같을 때까지 반복.
+            while (left <= right)
+            {
+                // 6. left가 right보다 작거나 같다면 반복 수행. pivot 값보다 left의 값이
+                //    작을 때 left 오른쪽 이동.
+
+                while (left <= end && array[pivot] >= array[left])
+                {
+                    left++;
+                }
+                // 7. rigth가 start보다 크다면 반복 수행. pivot 값보다 right 값이 클 때
+                //    right 왼쪽 이동.
+
+                while (start < right && array[pivot] <= array[right])
+                {
+                    right--;
+                }
+
+                if (left > right)
+                {
+                    Swap(ref array[pivot], ref array[right]);
+                }
+                else
+                {
+                    Swap(ref array[left], ref array[right]);
+                }
+            }
+
+            // 왼쪽 분할 정복 재귀 함수
+            QuickSort(array, start, right - 1);
+
+            // 오른쪽 분할 정복 재귀 함수
+            QuickSort(array, right + 1, end);
+        }
+
+        static void Main(string[] args)
+        {
+            #region 퀵 정렬
+
+            // 기준점을 획득한 다음 해당 기준점을 기준으로 배열을 나누고 한쪽에는
+            // 기준점보다 작은 항목들이 위치하고 다른 쪽에는 기준점보다 큰 항목들이 위치.
+
+            // 나뉘어진 하위 배열에 대해 재귀적으로 퀵 정렬을 호출하여 모든 배열이
+            // 기본 배열이 될 때까지 반복하면서 정렬하는 알고리즘.
+
+            // 시간복잡도 : O(log N)
+
+            int[] array = new int[9] { 5, 8, 3, 2, 1, 9, 4, 7, 6 };
+
+            QuickSort(array, 0, array.Length - 1);
+
+            foreach (int element in array)
+            {
+                Console.WriteLine(element);
+            }
+
+            // 5, 8, 3, 2, 1, 9, 4, 7, 6
+            // 5, 4, 3, 2, 1, 9, 4, 8, 6
+            // 1, 4, 3, 2, 5, 9, 4, 8, 6
+
+
+            #endregion
+
+
+        }
+    }
+}
--- kjwProgram/Program.cs
+++ kjwProgram/Program.cs
@@ -1,80 +1,48 @@
-﻿using System;
-using System.Security.Cryptography.X509Certificates;
-
-namespace kjwProgram
+﻿namespace kjwProgram
 {
+    public class A
+    {
+        public readonly int size5 = 8;
+    }
     internal class Program
     {
-        static void Swap(ref int x, ref int y)
+        static void MergeSort1(int[] array, int left, int right)
         {
-            int temp = x;
-            x = y;
-            y = temp;
+            if(left < right)
+            {
+                int mid = (left + right) / 2;
+                MergeSort1 (array, left, mid);
+                MergeSort1(array, mid + 1, right);
+                MergeSort2(array, left, mid, right);
+            }
         }
 
-        static void QuickSort(int[] array, int left, int right)
+        static void MergeSort2(int[] array, int left, int mid, int right)
         {
-            // 1. left가 right 보다 크거나 같아졌을 때
-            if (left >= right) return;
-
-            int pivot = left - 1;
-
-            while (left <= right)
-            {
-                while (array[pivot] >= array[left] && left <= right)
-                {
-                    left++;
-                    //if (left == array.Length - 1) break;
-                }
-                while (array[pivot] <= array[right] && pivot < right)
-                {
-                    right--;
-                    //if (right == pivot + 1) break;
-                }
-                if (left > right)
-                {
-                    //int temp = array[left];
-                    //array[left] = array[right];
-                    //array[right] = temp;
-                    Swap(ref array[pivot], ref array[right]);
-                }
-                else
-                {
-                    //int temp = array[pivot];
-                    //array[pivot] = array[right];
-                    //array[right] = temp;
-                    Swap(ref array[left], ref array[right]);
-
-                }
-            }
-            QuickSort(array, pivot + 1, right - 1);
-            QuickSort(array, right + 1, array.Length - 1);
+            int l, m, r;
+            l = left;
+            m = mid + 1;
+            r = right;
+            int 
         }
 
         static void Main(string[] args)
         {
-            #region 퀵 정렬
+            #region 병합 정렬
 
-            // 기준점을 획득한 다음 해당 기준점을 기준으로 배열을 나누고 한쪽에는
-            // 기준점보다 작은 항목들이 위치하고 다른 쪽에는 기준점보다 큰 항목들이 위치.
+            // 하나의 리스트를 두 개의 균등한 크기로 분할하고 분할된 부분 리스트를 정렬한 다음,
+            // 두 개의 정렬된 부분 리스트를 합하여 전체가 정렬된 리스트가 되게 하는 방법.
 
-            // 나뉘어진 하위 배열에 대해 재귀적으로 퀵 정렬을 호출하여 모든 배열이
-            // 기본 배열이 될 때까지 반복하면서 정렬하는 알고리즘.
+            // 분할 : 입력 배열을 같은 크기의 2개의 부분 배열로 분할.
+            // 정복 : 부분 배열을 정렬하며 부분 배열의 크기가 충분히 작지 않으면 
+            //        순환 호출을 이용하여 다시 분할 정복 실행.
+            // 결합 : 정렬된 부분 배열들을 하나의 배열에 병합.
+            A a = new A();
+            int[] array = new int[A.] { 10, 25, 37, 8, 3, 12, 41, 9 };
+            
 
-            // 시간복잡도 : O(log N)
 
-            int[] array = new int[9] { 5, 8, 3, 2, 1, 9, 4, 7, 6 };
 
-            QuickSort(array, 1, array.Length - 1);
-
-            foreach (int element in array)
-            {
-                Console.WriteLine(element);
-            }
-
-            // 5, 8, 3, 2, 1, 9, 4, 7, 6
-            // 5, 4, 3, 2, 1, 9, 4, 8, 6
-            // 1, 4, 3, 2, 5, 9, 4, 8, 6
 
 
             #endregion
