3e960cf8908e301c3013427bf24df507770b272f
hlf1 <139342292+kimjuwan134@users.noreply.github.com>
2023-11-27 19:46:18 +0900

    (2023.11.27) 그래프

---
+++ kjwClass13th/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace kjwClass13th
+{
+    internal class Program
+    {
+        static void MergeSort1(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int mid = (left + right) / 2;
+                MergeSort1(array, left, mid);
+                MergeSort1(array, mid + 1, right);
+                MergeSort2(array, left, mid, right);
+            }
+        }
+
+        static void MergeSort2(int[] array, int left, int mid, int right)
+        {
+            int[] a = new int[right + 1];
+            int l, m, k;
+            l = left;
+            m = mid + 1;
+            k = left;
+
+            while (l <= mid && m <= right)
+            {
+                if (array[l] <= array[m])
+                {
+                    a[k] = array[l];
+                    l++;
+                }
+                else
+                {
+                    a[k] = array[m];
+                    m++;
+                }
+                k++;
+            }
+            if (l > mid)
+            {
+                for (int t = m; t <= right; t++)
+                {
+                    a[k] = array[t];
+                    k++;
+                }
+            }
+            else
+            {
+                for (int t = l; t <= mid; t++)
+                {
+                    a[k] = array[t];
+                    k++;
+                }
+            }
+            for (int t = left; t <= right; t++)
+            {
+                array[t] = a[t];
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            #region 병합 정렬
+
+            // 하나의 리스트를 두 개의 균등한 크기로 분할하고 분할된 부분 리스트를 정렬한 다음,
+            // 두 개의 정렬된 부분 리스트를 합하여 전체가 정렬된 리스트가 되게 하는 방법.
+
+            // 분할 : 입력 배열을 같은 크기의 2개의 부분 배열로 분할.
+            // 정복 : 부분 배열을 정렬하며 부분 배열의 크기가 충분히 작지 않으면 
+            //        순환 호출을 이용하여 다시 분할 정복 실행.
+            // 결합 : 정렬된 부분 배열들을 하나의 배열에 병합.
+            //int[] array = new int[] { 10, 25, 37, 8, 3, 12, 41, 9 };
+            //
+            //MergeSort1(array, 0, array.Length - 1);
+            //for(int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine(array[i]);
+            //}
+
+            #endregion
+
+
+        }
+    }
+}
--- kjwProgram/Program.cs
+++ kjwProgram/Program.cs
@@ -2,79 +2,44 @@
 {
     internal class Program
     {
-        static void MergeSort1(int[] array, int left, int right)
-        {
-            if(left < right)
-            {
-                int mid = (left + right) / 2;
-                MergeSort1 (array, left, mid);
-                MergeSort1(array, mid + 1, right);
-                MergeSort2(array, left, mid, right);
-            }
-        }
-
-        static void MergeSort2(int[] array, int left, int mid, int right)
-        {
-            int[] a = new int[8];
-            int l, m, k;
-            l = left;
-            m = mid + 1;
-            k = left;
-            
-            while(l <= mid && m <=right)
-            {
-                if (array[l] <= array[m])
-                {
-                    a[k] = array[l];
-                    l++;
-                }
-                else
-                {
-                    a[k] = array[m];
-                    m++;
-                }
-                k++;
-            }
-            if (l > mid)
-            {
-                for(int t = m; t <= right; t++)
-                {
-                    a[k] = array[t];
-                    k++;
-                }
-            }
-            else
-            {
-                for(int t = l; t <= mid; t++)
-                {
-                    a[k] = array[t];
-                    k++;
-                }
-            }
-            for(int t = left; t <= right; t++)
-            {
-                array[t] = a[t];
-            }
-        }
-
         static void Main(string[] args)
         {
-            #region 병합 정렬
+            #region 그래프
 
-            // 하나의 리스트를 두 개의 균등한 크기로 분할하고 분할된 부분 리스트를 정렬한 다음,
-            // 두 개의 정렬된 부분 리스트를 합하여 전체가 정렬된 리스트가 되게 하는 방법.
+            // 정점(Vertex)과 간선(Edge)들이 서로 복잡하게 연결되어있는 자료구조.
 
-            // 분할 : 입력 배열을 같은 크기의 2개의 부분 배열로 분할.
-            // 정복 : 부분 배열을 정렬하며 부분 배열의 크기가 충분히 작지 않으면 
-            //        순환 호출을 이용하여 다시 분할 정복 실행.
-            // 결합 : 정렬된 부분 배열들을 하나의 배열에 병합.
-            int[] array = new int[8] { 10, 25, 37, 8, 3, 12, 41, 9 };
-            
-            MergeSort1(array, 0, array.Length - 1);
-            for(int i = 0; i < array.Length; i++)
-            {
-                Console.WriteLine(array[i]);
-            }
+            // 정점(Vertex)
+            // 노드(node)라고도 하며, 데이터가 저장되는 그래프의 기본 원소.
+
+            // 간선(Edge)
+            // 링크(link)라고도 하며, 정점간의 관계를 나타냄.
+
+            // 인접 정점(Adjacent Vertex)
+            // 하나의 정점에서 간선에 의해 직접 연결되어 있는 정점을 의미.
+
+            // 차수(Degree)
+            // 정점에 연결되어 있는 간선의 수를 의미.
+
+            // 진입 차수
+            // 외부에서 오는 간선의 수.
+
+            // 진출 차수
+            // 외부로 향하는 간선의 수.
+
+            // 단순 경로(Simple Path)
+            // 경로 중에서 반복되는 간선이 없는 경로를 의미.
+
+            // 사이클(Cycle)
+            // 시작 정점과 종료 정점이 같은, 단순 경로를 의미.
+
+
+
+
+
+            #endregion
+
+            #region 인접 행렬
+
 
 
 
